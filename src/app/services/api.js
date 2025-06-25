87d152f571f608249344e9ee8f455ac662a21584
shubhamsingh851 <shus@americana-food.com>
2025-06-25 18:06:31 +0530

    new

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create the axios instance
 const api = axios.create({
-    baseURL: "${process.env.NEXT_PUBLIC_API_URL}/api/",
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/`,
 });
 
 // Helper to set the auth token on all requests
