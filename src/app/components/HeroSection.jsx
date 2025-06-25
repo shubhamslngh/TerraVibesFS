c08c95475fc4ad3e826b92bc1af6723dcfafdac8
shubhamsingh851 <shus@americana-food.com>
2025-06-25 17:15:55 +0530

    date

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -1,45 +1,50 @@
 "use client";
 
 import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
 import TextGenerateEffect from "@/components/ui/TextGenerateEffect";
 
 export default function HeroSection() {
   const sectionRef = useRef(null);
 
+  // Real-time scroll detection
   const { scrollYProgress } = useScroll({
     target: sectionRef,
-    offset: ["start end", "center center"],
+    offset: ["start end", "center center"], 
   });
 
-  const scale = useTransform(scrollYProgress, [0, 0.3], [2.5, 1]);
+    const scale = useTransform(scrollYProgress, [0, 0.3], [2.5, 1]);
   const words = `Begin your healing with a journey...`;
 
   return (
-    <section
-      className="relative w-full h-[80vh] min-h-[500px] sm:min-h-[600px] bg-cover bg-center bg-no-repeat rounded-2xl flex flex-col items-center justify-center overflow-hidden"
-      style={{ backgroundImage: "url('/puneotw.jpeg')" }}>
+    <section  className="relative w-full h-[80vh]  mask-t-from-60% bg-[url('/puneotw.jpeg')] bg-cover bg-center rounded-2xl flex flex-col place-self-center items-center justify-center">
+
+      <motion.div
+        initial={{ scale: 2 }}
+        animate={{ scale: 1 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+        className="fixed inset-0  bg-no-repeat bg-contain bg-center"
+      />
+
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black/40 z-0" />
+      <div className="absolute inset-0 bg-black/30" />
 
       {/* Sticky Content */}
       <motion.div
         ref={sectionRef}
-        style={{ scale }}
-        initial={{ scale: 2 }}
+        initial={{ scale: 10 }}
         animate={{ scale: 1 }}
         transition={{ duration: 2, ease: "easeOut" }}
-        className="relative z-10 text-center max-w-4xl px-4 sm:px-6">
-        <motion.h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-cinzel font-bold tracking-wide leading-tight">
+        className="sticky top-1/2 -translate-y-1/2 z-10 text-center max-w-4xl mx-auto px-6">
+        <motion.h1 className="text-slate-900 text-6xl md:text-7xl font-cinzel font-bold tracking-wide">
           TERRAVIBES
         </motion.h1>
-
-        <TextGenerateEffect
-          className="mt-6 text-base sm:text-lg md:text-xl lg:text-2xl italic font-playfair text-gray-200"
-          duration={2}
-          filter={false}
-          words={words}
-        />
+      <TextGenerateEffect
+        className="mt-6 text-xl md:text-2xl italic font-playfair text-gray-200"
+        duration={2}
+        filter={false}
+        words={words}
+      />
       </motion.div>
     </section>
   );
